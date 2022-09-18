62f9dad2404eab8d95768071d6cf268a07628290
4ndr3steban <106042025+4ndr3steban@users.noreply.github.com>
2022-09-18 12:18:26 -0500

    zoologico

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -29,7 +29,7 @@ public class Animal {
 	public static String totalPorTipo() {
 		String str = "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" + 
 				"Aves: "+Ave.cantidadAves()+ "\n" + 
-				"Reptiles:"+ Reptil.cantidadReptiles() + "\n" + 
+				"Reptiles: "+ Reptil.cantidadReptiles() + "\n" + 
 				"Peces: "+ Pez.cantidadPeces() +"\n" + 
 				"Anfibios: "+ Anfibio.cantidadAnfibios();
 		return str;
